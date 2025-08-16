a60328359c86742b0376d287a5b55f55d99b5448
Neil Chadwick <neilchadders@hotmail.com>
2025-08-16 17:47:17 +0100

    add shiftsController

--- obj/Debug/net9.0/ShiftTrackerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShiftTrackerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShiftTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dadd1dfa8985ba2cdb81838c1897c95f196f11b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fa866c3f447f023d6a872b03676ae28f79480e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShiftTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShiftTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
